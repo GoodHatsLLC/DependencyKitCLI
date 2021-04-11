c0771b7b50500bc70c42bbd73f03f52365a52ccb
Adam Zethraeus <adam@zethrae.us>
2021-04-11 03:12:20 -0700

    add code to write generated files (poorly)

--- Sources/DependencyKitCLI/Utilities/FileSystem.swift
+++ Sources/DependencyKitCLI/Utilities/FileSystem.swift
@@ -11,6 +11,22 @@ class FileSystem {
             .filter { (try? $0.resourceValues(forKeys:[.isRegularFileKey]).isRegularFile) ?? false }
             ?? []
     }
+
+    static func writeFile(to url: URL, contents: String) {
+        if !FileManager.default.fileExists(atPath: url.absoluteString) {
+            do {
+                try FileManager.default
+                    .createDirectory(atPath: url.absoluteString, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                preconditionFailure("could not get or create file at: \(url)")
+            }
+        }
+        do {
+            try contents.write(to: url, atomically: true, encoding: .utf8)
+        } catch {
+            preconditionFailure("could not write to file at: \(url)")
+        }
+    }
 }
 
 typealias FS = FileSystem
--- Sources/DependencyKitCLI/main.swift
+++ Sources/DependencyKitCLI/main.swift
@@ -11,6 +11,11 @@ let readers = callConfiguration.modules.map {
     DeclarationAndImplementationReader(config: $0, displayDebugInfo: callConfiguration.displayDebugInfo)
 }
 let moduleDeclarations = readers.map { $0.parseModules() }
+if args.debugInfo {
+    moduleDeclarations.forEach {
+        print(String(describing: $0))
+    }
+}
 moduleDeclarations.forEach {
-    print(String(describing: $0))
+    FS.writeFile(to: $0.parsingConfig.codegenFile, contents: "/*\n"+String(describing: $0)+"\n*/\n")
 }
